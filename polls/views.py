b4df85539ec76b06826a6cd549c0ac928d591f2f
Stefan <stefsch@microsoft.com>
2019-10-23 09:29:58 -0700

    Edit to kickoff workflow
    Edit to kickoff workflow

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions.."""
+        """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
